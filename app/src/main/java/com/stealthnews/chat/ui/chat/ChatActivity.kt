19c5385a0159dd256456f71dadf4388985ebf4d6
50395493 <50395493@qq.com>
2026-05-08 15:40:09 +0800

    Update ChatActivity.kt

--- app/src/main/java/com/stealthnews/chat/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/stealthnews/chat/ui/chat/ChatActivity.kt
@@ -3,7 +3,7 @@ package com.stealthnews.chat.ui.chat
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.stealthnews.chat.data.local.AppDatabase
+import com.stealthnews.chat.data.local.database.AppDatabase
 import com.stealthnews.chat.data.local.dao.ChatMessageDao
 import com.stealthnews.chat.databinding.ActivityChatBinding
 import kotlinx.coroutines.CoroutineScope
