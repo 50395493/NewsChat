c5daeb553d9f55743bce9e0a811925b8db7320ac
50395493 <50395493@qq.com>
2026-05-08 10:04:10 +0800

    Create SportsNewsFragment.kt

---
+++ app/src/main/java/com/stealthnews/chat/ui/news/fragment/SportsNewsFragment.kt
@@ -0,0 +1,27 @@
+package com.stealthnews.chat.ui.news.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.stealthnews.chat.databinding.FragmentNewsListBinding
+
+class SportsNewsFragment : Fragment() {
+    private var _binding: FragmentNewsListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewsListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
