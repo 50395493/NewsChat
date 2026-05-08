2f5c8427d83a43b15894df0c1a593dc641dea137
50395493 <50395493@qq.com>
2026-05-08 10:03:39 +0800

    Create NewsPagerAdapter.kt

---
+++ app/src/main/java/com/stealthnews/chat/ui/news/NewsPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.stealthnews.chat.ui.news
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.stealthnews.chat.ui.news.fragment.TechNewsFragment
+import com.stealthnews.chat.ui.news.fragment.SportsNewsFragment
+import com.stealthnews.chat.ui.news.fragment.EntertainmentNewsFragment
+
+class NewsPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    
+    private val fragments = listOf(
+        TechNewsFragment(),
+        SportsNewsFragment(),
+        EntertainmentNewsFragment()
+    )
+
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
