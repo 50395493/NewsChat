466f06d72d63a4eb8388d108b8792a79f235b05e
50395493 <50395493@qq.com>
2026-05-14 10:15:18 +0800

    Fix SecureFileProvider constructor initialization

--- app/src/main/java/com/stealthnews/chat/provider/SecureFileProvider.kt
+++ app/src/main/java/com/stealthnews/chat/provider/SecureFileProvider.kt
@@ -2,7 +2,7 @@ package com.stealthnews.chat.provider
 
 import androidx.core.content.FileProvider
 
-class SecureFileProvider : FileProvider {
+class SecureFileProvider : FileProvider() {
     // FileProvider 用于安全地分享文件
     // 配置已在 @xml/file_paths 中定义
 }
